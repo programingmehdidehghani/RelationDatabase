23799da95382c639d8b973a0976e7e974cc2cc13
mehdi <dehghani_m@rocketmail.com>
2022-11-11 19:11:12 +0330

    query in one to one

--- app/src/main/java/com/example/relationdatabase/SchoolDao.kt
+++ app/src/main/java/com/example/relationdatabase/SchoolDao.kt
@@ -2,8 +2,11 @@ package com.example.relationdatabase
 
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
 import com.example.relationdatabase.entities.Director
 import com.example.relationdatabase.entities.School
+import com.example.relationdatabase.entities.relations.SchoolAndDirector
 
 interface SchoolDao {
 
@@ -12,4 +15,8 @@ interface SchoolDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDirector(director: Director)
+
+    @Transaction
+    @Query("SELECT * FROM school WHERE schoolName = :schoolName")
+    suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirector>
 }
