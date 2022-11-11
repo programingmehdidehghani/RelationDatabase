1869a8cad60b1686425e71c8799ab2cdb7e99f89
mehdi <dehghani_m@rocketmail.com>
2022-11-11 19:04:27 +0330

    start make insert

--- app/src/main/java/com/example/relationdatabase/SchoolDao.kt
+++ app/src/main/java/com/example/relationdatabase/SchoolDao.kt
@@ -1,4 +1,15 @@
 package com.example.relationdatabase
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.relationdatabase.entities.Director
+import com.example.relationdatabase.entities.School
+
 interface SchoolDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSchool(school: School)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDirector(director: Director)
 }
