8c9e2ba148a109bb4285583ec887ef42e979d17b
mehdi <dehghani_m@rocketmail.com>
2022-11-11 18:49:22 +0330

    make package relations

--- app/src/main/java/com/example/relationdatabase/entities/School.kt
+++ app/src/main/java/com/example/relationdatabase/entities/School.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-class Director (
+class School (
     @PrimaryKey(autoGenerate = false)
-   val directorName : String
+    val schoolName: String
 )
