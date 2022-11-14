692f9504638ee227befdf1a93d151716eac1d6da
mehdi <dehghani_m@rocketmail.com>
2022-11-14 20:57:30 +0330

    relation one to many

--- app/src/main/java/com/example/relationdatabase/SchoolDao.kt
+++ app/src/main/java/com/example/relationdatabase/SchoolDao.kt
@@ -6,7 +6,9 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.example.relationdatabase.entities.Director
 import com.example.relationdatabase.entities.School
+import com.example.relationdatabase.entities.Student
 import com.example.relationdatabase.entities.relations.SchoolAndDirector
+import com.example.relationdatabase.entities.relations.SchoolWithStudent
 
 interface SchoolDao {
 
@@ -16,7 +18,14 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDirector(director: Director)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(student: Student)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirector>
+
+    @Transaction
+    @Query("SELECT * FROM school WHERE schoolName = :schoolName")
+    suspend fun getSchoolWithStudents(schoolName: String) : List<SchoolWithStudent>
 }
--- app/src/main/java/com/example/relationdatabase/entities/relations/SchoolWithStudent.kt
+++ app/src/main/java/com/example/relationdatabase/entities/relations/SchoolWithStudent.kt
@@ -1,6 +1,18 @@
 package com.example.relationdatabase.entities.relations
 
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.example.relationdatabase.entities.School
+import com.example.relationdatabase.entities.Student
 
-data class SchoolWithStudent {
 
-}
+data class SchoolWithStudent(
+    @Embedded val school: School,
+    @Relation(
+        parentColumn = "schoolName",
+        entityColumn = "schoolName"
+    )
+    val student: List<Student>
+
+)
+
